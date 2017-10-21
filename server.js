3210890b8bb39f9f1b093d29a5b05e30a64d45f7
jwats287 <jwats287@users.noreply.github.com>
2017-10-21 13:36:29 -0400

    Fixed outdated error message

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
@@ -9,22 +10,38 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const connection = mongoose.connection;
 
 const configDB = require('./config/database.js');
 
-mongoose.connect(configDB.url)
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+connection.on('connected', () => {
+    console.log('Mongoose Connected Successfully');
+});
+
+connection.on('error', (err) => {
+    console.log(`Mongoose default connection error: ${err}`)
+})
+
+// require('./config/passport')(passport);
 
 app.use(morgan('dev'));
 app.use(cookieParser());
-app.use(bodyParser());
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
-app.use(session({secret: 'changethistosomething' }));
+app.use(session({
+    secret: 'changethistosomething',
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
 require('./app/routes.js')(app, passport);
 
-app.listen(port);
-console.log(`The magic happens on port ${port}`);
+app.listen(port, () => {
+    console.log(`Magic is happening on port ${port}`)
+})
